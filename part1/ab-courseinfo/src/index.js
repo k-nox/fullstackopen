dc46d8677a0750b30ec8f296935b4a63a45e14a7
knox <lux.barker13@gmail.com>
2021-02-09 16:19:34 -0800

    Exercise 1.4 - removed comment

--- part1/ab-courseinfo/src/index.js
+++ part1/ab-courseinfo/src/index.js
@@ -67,19 +67,3 @@ const App = () => {
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-/* 
-      <Content
-        part1={parts[0].name}
-        part2={parts[1].name}
-        part3={parts[2].name}
-        exercises1={parts[0].exercises}
-        exercises2={parts[1].exercises}
-        exercises3={parts[2].exercises}
-      />
-      <Total
-        exercises1={parts[0].exercises}
-        exercises2={parts[1].exercises}
-        exercises3={parts[2].exercises}
-      />
-*/
