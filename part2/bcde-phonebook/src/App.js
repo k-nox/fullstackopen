077510caf45c09bcffed46a0924b3bf9a9db66b3
knox <lux.barker13@gmail.com>
2021-02-12 18:32:17 -0800

    Exercise 2.8

--- part2/bcde-phonebook/src/App.js
+++ part2/bcde-phonebook/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', phone: '123-456-7890' }]);
   const [newName, setNewName] = useState('');
+  const [newPhoneNumber, setNewPhoneNumber] = useState('');
 
   const addName = (e) => {
     e.preventDefault();
@@ -12,15 +13,21 @@ const App = () => {
     } else {
       const personObject = {
         name: newName,
+        phone: newPhoneNumber,
       };
 
       setPersons(persons.concat(personObject));
       setNewName('');
+      setNewPhoneNumber('');
     }
   };
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handlePhoneChange = (e) => {
+    setNewPhoneNumber(e.target.value);
   };
 
   return (
@@ -31,14 +38,21 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          phone: <input value={newPhoneNumber} onChange={handlePhoneChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.phone}
+        </p>
       ))}
-      <div>debug: {newName}</div>
+      <div>
+        debug: {newName} {newPhoneNumber}
+      </div>
     </div>
   );
 };
