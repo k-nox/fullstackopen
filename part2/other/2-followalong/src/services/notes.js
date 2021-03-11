174e22d47580926604952da9063d2af496d479b8
knox <lux.barker13@gmail.com>
2021-03-10 16:46:38 -0800

    part 3 followalong

--- part2/other/2-followalong/src/services/notes.js
+++ part2/other/2-followalong/src/services/notes.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
-const baseURL = 'http://localhost:3001/notes';
-
-/* const getAll = () => {
-  const request = axios.get(baseURL);
-  return request.then((response) => response.data);
-}; */
+const baseURL = 'http://localhost:3001/api/notes';
 
 const getAll = () => {
   const request = axios.get(baseURL);
