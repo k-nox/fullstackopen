4e8161031837cc63cf991a7c16046184712a57ca
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-07 14:31:57 -0800

    destructuring

--- part1/c-component-state-event-handlers/hello/src/App.jsx
+++ part1/c-component-state-event-handlers/hello/src/App.jsx
@@ -1,20 +1,24 @@
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
   const name = 'Biscuit'
-  const age = 2
+  const age = 3
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Maggie" age={1 + 2} />
+      <Hello name="Maggie" age={1 + 3} />
       <Hello name={name} age={age} />
-      <Hello name="Tiny" age={1} />
+      <Hello name="Tiny" age={2} />
     </div>
   )
 }
