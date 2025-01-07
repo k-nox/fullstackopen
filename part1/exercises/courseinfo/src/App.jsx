35bedf77dd99bc0b7b4c035eedfc891b3b1d498a
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-07 11:22:31 -0800

    complete exercise 1.1

--- part1/exercises/courseinfo/src/App.jsx
+++ part1/exercises/courseinfo/src/App.jsx
@@ -1,3 +1,7 @@
+import Content from "./Content"
+import Header from "./Header"
+import Total from "./Total"
+
 export const App = () => {
   const course = 'Half stack application development'
   const part1 = 'Fundamentals of React'
@@ -8,17 +12,9 @@ export const App = () => {
   const exercises3 = 14
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
