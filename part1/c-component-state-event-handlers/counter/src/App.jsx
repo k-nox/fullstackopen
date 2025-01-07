bb5ac47bb67b18f5cb46e9db2e8d7ffa278ca528
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-07 14:49:27 -0800

    use event handlers to change state

--- part1/c-component-state-event-handlers/counter/src/App.jsx
+++ part1/c-component-state-event-handlers/counter/src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from "react"
 
 const App = () => {
   const [counter, setCounter] = useState(0)
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
 
-  console.log('rendering...', counter)
+  const increaseByOne = () => setCounter(counter + 1)
+  
+  const setToZero = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}>
+        zero
+      </button>
+    </>
   )
 }
 export default App
