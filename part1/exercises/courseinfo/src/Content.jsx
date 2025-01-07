f7ccd8db4a0d9c5b69ea4961ebfbef213f581db0
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-07 13:35:16 -0800

    complete exercise 1.5

--- part1/exercises/courseinfo/src/Content.jsx
+++ part1/exercises/courseinfo/src/Content.jsx
@@ -3,9 +3,9 @@ import Part from "./Part"
 const Content = (props) => {
   return (
     <>
-        <Part part={props.parts[0]} />
-        <Part part={props.parts[1]}  />
-        <Part part={props.parts[2]}  />
+        <Part part={props.course.parts[0]} />
+        <Part part={props.course.parts[1]}  />
+        <Part part={props.course.parts[2]}  />
     </>
   )
 }
