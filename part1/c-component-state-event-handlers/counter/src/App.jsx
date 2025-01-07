84c3c82d3f84ea422cac7cf045052e388d94ea3c
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-07 14:54:28 -0800

    pass down state to child props

--- part1/c-component-state-event-handlers/counter/src/App.jsx
+++ part1/c-component-state-event-handlers/counter/src/App.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react"
+import Display from "./Display"
+import Button from "./Button"
 
 const App = () => {
   const [counter, setCounter] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
-  
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button 
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button 
+        onClick={decreaseByOne}
+        text='minus'
+      />
+      <Button 
+        onClick={setToZero}
+        text='zero'
+      />
     </>
   )
 }
