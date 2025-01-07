92d082d83838aa33f92b1f63d5a5c9f45ce3d100
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-07 10:53:20 -0800

    use props

--- part1/a-intro-to-react/introdemo/src/App.jsx
+++ part1/a-intro-to-react/introdemo/src/App.jsx
@@ -1,15 +1,18 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a + b)
-
+const Hello = props => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <p>Hello {props.name} </p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maggie'/>
+      <Hello name='Biscuit'/>
+      <Hello name='Tiny'/>
     </div>
   )
 }
