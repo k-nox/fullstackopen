9d9cf14ff3974bb7ab5987a06f52ee4b294816e5
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-07 15:05:44 -0800

    simplify components

--- part1/c-component-state-event-handlers/counter/src/Button.jsx
+++ part1/c-component-state-event-handlers/counter/src/Button.jsx
@@ -1,8 +1,3 @@
-const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>
-        {text}
-    </button>
-  )
-}
+const Button = ({ onClick, text }) => <button onClick={onClick}> {text} </button>
+
 export default Button
