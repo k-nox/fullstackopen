16afbc2270b09b2d935a0ecb905c871df49bf4e0
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-08 11:08:21 -0800

    filter notes

--- part2/b-forms/notes/src/App.jsx
+++ part2/b-forms/notes/src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 const App = ({ initialNotes }) => {
   const [notes, setNotes] = useState(initialNotes);
   const [newNote, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -21,11 +22,18 @@ const App = ({ initialNotes }) => {
     setNewNote(event.target.value);
   };
 
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
