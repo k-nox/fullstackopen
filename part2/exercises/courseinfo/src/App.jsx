d102ea4b18720ba55753e0e06813690f97cc9969
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-08 10:41:08 -0800

    complete exercise 2.4

--- part2/exercises/courseinfo/src/App.jsx
+++ part2/exercises/courseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-const Header = ({ text }) => <h1>{text}</h1>;
+const Header = ({ text }) => <h2>{text}</h2>;
 
 const Total = ({ parts }) => {
   const sum = parts.reduce((sum, part) => {
@@ -36,34 +36,61 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  const courseComponents = courses.map((course) => (
+    <Course key={course.id} course={course} />
+  ));
+
+  return (
+    <>
+      <h1>Web devlopment curriculum</h1>
+      {courseComponents}
+    </>
+  );
 };
 
 export default App;
