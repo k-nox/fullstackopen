cc66d2cad386771ab0188d375a2a7f36af0ffd44
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-09 12:40:00 -0800

    toggle importance of notes

--- part2/d-altering-data-in-server/notes/src/App.jsx
+++ part2/d-altering-data-in-server/notes/src/App.jsx
@@ -34,6 +34,16 @@ const App = () => {
     setNewNote(event.target.value);
   };
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((n) => (n.id === id ? response.data : n)));
+    });
+  };
+
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
@@ -46,7 +56,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
