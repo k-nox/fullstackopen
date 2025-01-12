7288d3430d88ed0c263b4b4c98a50bb040454098
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-12 09:18:32 -0800

    connect to local mongo

---
+++ part3/c-mongodb/backend/mongo.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb://fullstackopen:${password}@127.0.0.1:27017/?authSource=admin&retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is easy",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
