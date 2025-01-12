48c9c579fbecb296d5391060839b4e5c0815fcf8
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-12 10:13:23 -0800

    connect to remote mongodb

--- part3/c-mongodb/backend/mongo.js
+++ part3/c-mongodb/backend/mongo.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
+  console.log("give password and as argument");
   process.exit(1);
 }
 
 const password = process.argv[2];
+const env = process.argv.length < 4 ? "dev" : process.argv[3];
 
-const url = `mongodb://fullstackopen:${password}@127.0.0.1:27017/?authSource=admin&retryWrites=true&w=majority`;
+const urls = {
+  dev: `mongodb://fullstackopen:${password}@127.0.0.1:27017/noteApp?authSource=admin&retryWrites=true&w=majority`,
+  prod: `mongodb+srv://fullstackopen:${password}@fullstackopen.9kdef.mongodb.net/noteApp?retryWrites=true&w=majority&appName=FullStackOpen`,
+};
 
 mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.connect(urls[env]);
 
 const noteSchema = new mongoose.Schema({
   content: String,
@@ -19,12 +23,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-const note = new Note({
-  content: "HTML is easy",
-  important: true,
-});
+// const note = new Note({
+//   content: "HTML is easy",
+//   important: true,
+// });
+//
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
 
-note.save().then((result) => {
-  console.log("note saved!");
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
   mongoose.connection.close();
 });
