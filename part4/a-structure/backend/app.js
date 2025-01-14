e1ccaedcab1a3e7f29d30d79e803f72bd7403651
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-14 09:04:35 -0800

    restructure backend

---
+++ part4/a-structure/backend/app.js
@@ -0,0 +1,31 @@
+import config from './utils/config.js'
+import express from 'express'
+import cors from 'cors'
+import notesRouter from './controllers/notes.js'
+import middleware from './utils/middleware.js'
+import logger from './utils/logger.js'
+import mongoose from 'mongoose'
+
+const app = express()
+
+mongoose.set('strictQuery', false)
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('conneced to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connecting to MongoDB:', error.message)
+	})
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+export default app
---
+++ part4/a-structure/backend/index.js
@@ -0,0 +1,7 @@
+import app from './app.js'
+import logger from './utils/logger.js'
+import config from './utils/config.js'
+
+app.listen(config.PORT, () => {
+	logger.info(`Server running on port ${config.PORT}`)
+})
