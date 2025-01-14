9b4ea925349f2661427fee0a68aee4b8e2893e74
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-14 15:32:11 -0800

    add tests for fetching and deleting single notes

--- part4/b-testing-backend/backend/tests/note_api.test.js
+++ part4/b-testing-backend/backend/tests/note_api.test.js
@@ -71,6 +71,32 @@ describe('notes', async () => {
 		assert.strictEqual(notesAtEnd.length, initialNotes.length)
 	})
 
+	test('a specific note can be viewed', async () => {
+		const notesAtStart = await notesInDb()
+
+		const noteToView = notesAtStart[0]
+
+		const resultNote = await api
+			.get(`/api/notes/${noteToView.id}`)
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+
+		assert.deepStrictEqual(resultNote.body, noteToView)
+	})
+
+	test('a note can be deleted', async () => {
+		const notesAtStart = await notesInDb()
+		const noteToDelete = notesAtStart[0]
+
+		await api.delete(`/api/notes/${noteToDelete.id}`).expect(204)
+
+		const notesAtEnd = await notesInDb()
+
+		const contents = notesAtEnd.map((r) => r.content)
+		assert(!contents.includes(noteToDelete.content))
+		assert.strictEqual(notesAtEnd.length, initialNotes.length - 1)
+	})
+
 	after(async () => {
 		await mongoose.connection.close()
 		await mongoServer.stop()
