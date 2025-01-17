c4d817a8c6f71d4a31a6552b23b9aba15a46ded2
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-01-17 15:36:03 -0800

    populate users on notes and vice versa

--- part4/c-user-admin/backend/controllers/notes.js
+++ part4/c-user-admin/backend/controllers/notes.js
@@ -5,12 +5,15 @@ import User from '../models/user.js'
 const notesRouter = Router()
 
 notesRouter.get('/', async (_request, response) => {
-	const notes = await Note.find({})
+	const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
 	response.json(notes)
 })
 
 notesRouter.get('/:id', async (request, response) => {
-	const note = await Note.findById(request.params.id)
+	const note = await Note.findById(request.params.id).populate('user', {
+		username: 1,
+		name: 1,
+	})
 	if (note) {
 		response.json(note)
 	} else {
